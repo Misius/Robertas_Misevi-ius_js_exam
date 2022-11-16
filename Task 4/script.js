6f0258d9a3da08e9e20ca2da20a7cfe916d4b82d
Robertas Misevičius <robertas31810@gmail.com>
2022-11-16 11:33:30 +0200

    Task 4

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,3 +9,33 @@ būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+
+function getData() {
+    fetch(ENDPOINT)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            for (let i = 0; i < data.length; i++) {
+                document.getElementById('output').innerHTML +=
+                ` <div class = 'card'><h3>${data[i].brand}</h3><br> ${data[i].models}</div>`;
+            }
+        })
+        .catch((err) => console.warn('klaida getData', err));
+}
+getData();
+
+
+function makeCard(obj) { 
+    const divEl = document.createElement('div');
+    divEl.className = 'card';
+    const h3El = document.createElement('h3');
+    h3El.textContent = `${data[obj].brand}`;
+    const pEl = document.createElement('p');
+    pEl.textContent = `${data[obj].models }`;
+    imgEl.src = obj.avatar_url;
+    imgEl.alt = obj.login;
+    
+    divEl.append(h3El, pEl);
+    return divEl;
+}
